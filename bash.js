0b280208e90c87d80574efba12cbe4d2f59fbb7b
Jungsoo Bin <witberrybean@gmail.com>
2018-06-12 14:16:59 -0400

    jungsooSwitch

--- bash.js
+++ bash.js
@@ -1,12 +1,21 @@
+
+const pwd = require('./pwd');
+const ls = require('./ls');
+// if a user enters 'pwd' as an argument, we can then call it
+
+
 process.stdout.write('prompt > ');
 
-process.stdin.on('data',(data) => {
-  const cmd = data.toString().trim();
-  if (cmd === 'pwd'){
-    process.stdout.write(`${process.cwd()}`)
-  } else {
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt >');
-  }
+    process.stdin.on('data',(data) => {
+    const cmd = data.toString().trim();
+    if (cmd === 'pwd'){
+      pwd()
+    } else if (cmd === 'ls') {
+      ls()
+    }
+    else {
+        process.stdout.write('You typed: ' + cmd);
+        process.stdout.write('\nprompt >');
+    }
 
-});
+    });
---
+++ ls.js
@@ -0,0 +1,19 @@
+
+
+
+    const fs = require('fs');
+    
+
+    const fsreaddir = fs.readdir('./', 'utf8', (err, files) => {
+        if (err) {
+            throw err
+        } else {
+            process.stdout.write(files.join('\n'))
+            process.stdout.write("prompt > ");
+    
+        }
+    })
+
+    process.stdout.write(`${fsreaddir}`)
+
+
